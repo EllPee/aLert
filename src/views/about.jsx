11da2edeed232896f1d088b3fa6d3f7423cb54f6
Weipeng Wu <weipeng.wu@student.fh-kiel.de>
2023-09-26 22:27:41 +0200

    Über alert und Kontakt

--- src/views/about.jsx
+++ src/views/about.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function About() {
+    const [iframeHeight, setIframeHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIframeHeight(window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div>
-            <h1>Über alert</h1>
-            <p>Dies ist die Über alert Seite.</p>
+        <div style={{width: '100%', height: iframeHeight}}>
+            <iframe
+                src="https://www.h3-basa-maps.de/alert/"
+                width="100%"
+                height="100%"
+                title="Embedded Alert Page"
+            ></iframe>
         </div>
     );
 }
